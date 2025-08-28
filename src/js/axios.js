ed3c56effbe3677733ee7e34e4dc8b40474a713c
EvgBck <grandblacklist@gmail.com>
2025-08-28 23:41:18 +0300

    add changes

---
+++ src/js/axios.js
@@ -0,0 +1,2 @@
+import axios from 'axios';
+axios.defaults.baseURL = 'https://sound-wave.b.goit.study/api';
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Define a FontFace
+const font = new FontFace("my-font", 'url("my-font.woff")', {
+  weight: "400 500 600 700",
+  stretch: "condensed",
+});
+
+// Add to the document.fonts (FontFaceSet)
+document.fonts.add(font);
