cfc1dc23875d3184706ae29e6d0998a845cd1695
EvgBckAccount <grandblacklist@gmail.com>
2025-09-02 00:50:29 +0300

    Update render.js

--- src/js/render.js
+++ src/js/render.js
@@ -194,12 +194,13 @@ function createAlbumHTML(album) {
       ${tracksHTML}
     </li>
   `;
-
+}
 // src/js/render.js
-export function showLoader() {
-  document.querySelector('.loader-container')?.classList.remove('hidden');
-}
+//export function showLoader() {
+//  document.querySelector('.loader-container')?.classList.remove('hidden');
+//}
 
-export function hideLoader() {
-  document.querySelector('.loader-container')?.classList.add('hidden');
-}
+//export function hideLoader() {
+ // document.querySelector('.loader-container')?.classList.add('hidden');
+
+//}
