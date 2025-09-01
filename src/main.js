195907e7377c7e56d51f49c2b1cd81b718afa863
EvgBckAccount <grandblacklist@gmail.com>
2025-09-02 00:53:19 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,1 @@
-// Define a FontFace
-const font = new FontFace("my-font", 'url("my-font.woff")', {
-  weight: "400 500 600 700",
-  stretch: "condensed",
-});
 
-// Add to the document.fonts (FontFaceSet)
-document.fonts.add(font);
-
-import './js/artists.js';
